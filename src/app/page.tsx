2d5157b7adf303ae2a8cad8364a1df540955bd0c
marcomz <marco@MacBook-Pro-de-Marco.local>
2025-11-18 04:55:39 -0700

    bug: Fix incorrect declaration
    Use const instead of let for a unchanging value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Catalog from "../components/Catalog.tsx";
 
 export default async function Home({ searchParams }) {
   const genre = (await searchParams).genre;
-  let url = `${process.env.PROJECT_URL}/api/games${genre ? `?genre=${genre}` : ''}`;
+  const url = `${process.env.PROJECT_URL}/api/games${genre ? `?genre=${genre}` : ''}`;
   const data = await fetch(url);
   const result = await data.json();
 
